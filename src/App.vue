8e398eb3bf5a6917c6f5a39dcc5605348b14d73c
alienorder <alienorder.dev@gmail.com>
2023-11-08 08:58:32 +0900

    허위매물 신고 추가

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,7 @@
   <div v-for="(product, i) in products" :key="i">
     <h4>{{ products[i] }}</h4>
     <p>50 만원</p>
+    <button @click="increase(i)">허위매물신고</button> <span>신고수 : {{ 신고수[i] }}</span>
   </div>
 
 </template>
@@ -18,6 +19,12 @@ export default {
     return {
       메뉴들 : ['Home', 'Shop', 'About'],
       products : ['역삼동원룸', '천호동원룸', '마포구원룸'],
+      신고수 : [0, 0, 0],
+    }
+  },
+  methods: {
+    increase(i){
+      this.신고수[i] += 1;
     }
   },
   components: {
